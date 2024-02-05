4cf2e7054d21cb6dc88ac5afa19b8b2f7637c555
Dale-WM <jrlim@wemade.com>
2024-02-05 15:28:47 +0900

    Fex: add lint-staged with husky #1

--- src/types/PlayWalletProviderType.ts
+++ src/types/PlayWalletProviderType.ts
@@ -2,7 +2,15 @@ import { BalanceType } from './PlayWalletSdkType';
 
 interface PlayWalletProviderService {
   getBalanceAll: () => Promise<Array<BalanceType>>;
-  getBalanceOf: ({ chainName, contractName, contractAddress }: { chainName: string; contractName: string; contractAddress: string }) => Promise<unknown>;
+  getBalanceOf: ({
+    chainName,
+    contractName,
+    contractAddress
+  }: {
+    chainName: string;
+    contractName: string;
+    contractAddress: string;
+  }) => Promise<unknown>;
 }
 
 export type { PlayWalletProviderService };
